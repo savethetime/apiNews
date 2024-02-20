19849d0aaadb8423623540905583a72b045b4e1a
savethetime <stopwatchstudy@gmail.com>
2024-02-21 03:20:06 +0900

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY=`7188006103df4e1dbdeec801d3df41c1`;
 let news = [];
 const getLatestNews = async ()=>{
-    const url= new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    const url= new URL(`https://remarkable-llama-5fada7.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();

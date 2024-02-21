3c7ca579aae8fa396e2b63f920b7c9a7c1f325c7
savethetime <stopwatchstudy@gmail.com>
2024-02-22 01:09:33 +0900

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 const API_KEY=`7188006103df4e1dbdeec801d3df41c1`;
-let news = [];
+let newsList = [];
 const getLatestNews = async ()=>{
     const url= new URL(`https://remarkable-llama-5fada7.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("ddd", news);
+    newsList = data.articles;
+    render();
+    console.log("ddd", newsList);
+};
+
+const render=()=>{
+    const newsHTML = newsList.map(
+        (news) => `<div class="row news">
+    <div class="col-lg-4">
+        <image class="news-img-size" 
+        src="${news.urlToImage}"/>
+   </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>
+           ${news.description}
+        </p>
+        <div>
+            ${news.source.name} * ${news.publishedAt}
+        </div>
+
+    </div>
+</div>`
+).join('');
+console.log("html", newsHTML)
+    document.getElementById('news-board').innerHTML=newsHTML;
 };
 
 getLatestNews();
